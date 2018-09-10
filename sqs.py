0f67f28f1ada355387223cd6149850eb94c536d5
Ryne Yang <linehrr@gmail.com>
2018-09-10 13:33:13 -0400

    init commit
     Changes to be committed:
    	new file:   sqs.py

---
+++ sqs.py
@@ -0,0 +1,30 @@
+conf = {
+  "sqs-access-key": "",
+  "sqs-secret-key": "",
+  "reader-sqs-queue": "",
+  "writer-sqs-queue": "",
+  "message-group-id": ""
+}
+
+import boto3
+client = boto3.client(
+	'sqs',
+        aws_access_key_id       = conf.get('sqs-access-key'),
+        aws_secret_access_key   = conf.get('sqs-secret-key')
+)
+
+while True:
+	messages = client.receive_message(QueueUrl=conf['reader-sqs-queue'], MaxNumberOfMessages=10)
+
+	if 'Messages' in messages:
+		for m in messages['Messages']:
+			print(m)
+			client.send_message(
+			  QueueUrl=conf['writer-sqs-queue'],
+			  MessageBody=m['Body'],
+			  MessageGroupId=conf['message-group-id']
+			)
+			client.delete_message(QueueUrl=conf['reader-sqs-queue'], ReceiptHandle=m['ReceiptHandle'])
+	else:
+		print('Queue is currently empty or messages are invisible')
+		break
